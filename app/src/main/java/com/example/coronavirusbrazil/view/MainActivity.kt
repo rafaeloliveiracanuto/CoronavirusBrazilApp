65cef298e6d9b9d6af341c9dbdaf8a94c63c4bc6
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-05 15:01:54 -0300

    Fix

--- app/src/main/java/com/example/coronavirusbrazil/view/MainActivity.kt
+++ app/src/main/java/com/example/coronavirusbrazil/view/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+            R.id.nav_north, R.id.nav_northeast, R.id.nav_south, R.id.nav_southeast), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
