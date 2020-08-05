1688120e861c68e09e75b6158de17f96cd82824b
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-05 15:19:36 -0300

    Add text as LiveData

--- app/src/main/java/com/example/coronavirusbrazil/viewModel/SoutheastViewModel.kt
+++ app/src/main/java/com/example/coronavirusbrazil/viewModel/SoutheastViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.coronavirusbrazil.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SoutheastViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is southeast Fragment"
+    }
+    val text: LiveData<String> = _text
 }
