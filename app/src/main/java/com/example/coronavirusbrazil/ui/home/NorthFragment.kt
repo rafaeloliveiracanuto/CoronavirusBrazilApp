9c595086252c77a8c464ee7c47a60da4b733f1bc
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-05 14:44:49 -0300

    Rename layouts

--- app/src/main/java/com/example/coronavirusbrazil/ui/home/NorthFragment.kt
+++ app/src/main/java/com/example/coronavirusbrazil/ui/home/NorthFragment.kt
@@ -21,7 +21,7 @@ class NorthFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_north, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
--- app/src/main/java/com/example/coronavirusbrazil/ui/slideshow/SouthFragment.kt
+++ app/src/main/java/com/example/coronavirusbrazil/ui/slideshow/SouthFragment.kt
@@ -21,7 +21,7 @@ class SouthFragment : Fragment() {
     ): View? {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val root = inflater.inflate(R.layout.fragment_northeast, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
