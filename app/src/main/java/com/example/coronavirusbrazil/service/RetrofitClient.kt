525b5281aea3ea0b3ec382fc3fab5e9dc594604c
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 11:11:51 -0300

    Finish getRetrofitInstance function

--- app/src/main/java/com/example/coronavirusbrazil/service/RetrofitClient.kt
+++ app/src/main/java/com/example/coronavirusbrazil/service/RetrofitClient.kt
@@ -1,7 +1,9 @@
 package com.example.coronavirusbrazil.service
 
 import com.example.coronavirusbrazil.service.constants.Constants
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitClient private constructor() {
     companion object {
@@ -9,7 +11,17 @@ class RetrofitClient private constructor() {
         private val BASE_URL = Constants.RETROFIT.BASE_URL
 
         private fun getRetrofitInstance(): Retrofit {
-            
+            val httpClient = OkHttpClient.Builder()
+
+            if (!::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return retrofit
         }
     }
 }
