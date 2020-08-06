87d84a188b12058fe3665b9ebabd11aeba032252
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 11:57:01 -0300

    Add UF object

--- app/src/main/java/com/example/coronavirusbrazil/service/constants/Constants.kt
+++ app/src/main/java/com/example/coronavirusbrazil/service/constants/Constants.kt
@@ -4,4 +4,34 @@ class Constants private constructor(){
     object RETROFIT {
         const val BASE_URL = "https://covid19-brazil-api.now.sh"
     }
+
+    object UF {
+        const val RO = "ro"
+        const val AC = "ac"
+        const val AM = "am"
+        const val RR = "rr"
+        const val PA = "pa"
+        const val AP = "ap"
+        const val TO = "to"
+        const val MA = "ma"
+        const val PI = "pi"
+        const val CE = "ce"
+        const val RN = "rn"
+        const val PB = "pb"
+        const val PE = "pe"
+        const val AL = "al"
+        const val SE = "se"
+        const val BA = "ba"
+        const val MG = "mg"
+        const val ES = "es"
+        const val RJ = "rj"
+        const val SP = "sp"
+        const val PR = "pr"
+        const val SC = "sc"
+        const val RS = "rs"
+        const val MT = "mt"
+        const val GO = "go"
+        const val DF = "df"
+
+    }
 }
