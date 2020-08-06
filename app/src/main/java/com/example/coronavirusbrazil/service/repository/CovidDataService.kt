22731cb069e379d0c6167340dec10d8f88f84acc
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 11:46:42 -0300

    Add getData function

--- app/src/main/java/com/example/coronavirusbrazil/service/repository/CovidDataService.kt
+++ app/src/main/java/com/example/coronavirusbrazil/service/repository/CovidDataService.kt
@@ -1,5 +1,12 @@
 package com.example.coronavirusbrazil.service.repository
 
+import com.example.coronavirusbrazil.service.model.CovidDataModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface CovidDataService {
 
+    @GET("/api/report/v1/brazil/uf/{uf}")
+    fun getData(@Path(value = "uf", encoded = true) uf: Int): Call<CovidDataModel>
 }
