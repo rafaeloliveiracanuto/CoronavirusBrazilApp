4b586ff3d37adcf2148e684e3a752a62c2e26b95
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 11:04:03 -0300

    Create getRetrofitInstance function

--- app/src/main/java/com/example/coronavirusbrazil/service/RetrofitClient.kt
+++ app/src/main/java/com/example/coronavirusbrazil/service/RetrofitClient.kt
@@ -1,5 +1,15 @@
 package com.example.coronavirusbrazil.service
 
-class RetrofitClient {
+import com.example.coronavirusbrazil.service.constants.Constants
+import retrofit2.Retrofit
 
+class RetrofitClient private constructor() {
+    companion object {
+        private lateinit var retrofit: Retrofit
+        private val BASE_URL = Constants.RETROFIT.BASE_URL
+
+        private fun getRetrofitInstance(): Retrofit {
+            
+        }
+    }
 }
