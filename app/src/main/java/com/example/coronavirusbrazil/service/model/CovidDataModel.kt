76157e9168ba6a74b0aa31df91d8535f75c4b252
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 11:32:58 -0300

    Add serializedNames

--- app/src/main/java/com/example/coronavirusbrazil/service/model/CovidDataModel.kt
+++ app/src/main/java/com/example/coronavirusbrazil/service/model/CovidDataModel.kt
@@ -1,5 +1,31 @@
 package com.example.coronavirusbrazil.service.model
 
+import com.google.gson.annotations.SerializedName
+
 class CovidDataModel {
-    
+
+    @SerializedName("uid")
+    var id: Int = 0
+
+    @SerializedName("uf")
+    var uf: String = ""
+
+    @SerializedName("state")
+    var state: String = ""
+
+    @SerializedName("cases")
+    var cases: Int = 0
+
+    @SerializedName("deaths")
+    var deaths: Int = 0
+
+    @SerializedName("suspects")
+    var suspects: Int = 0
+
+    @SerializedName("refuses")
+    var refuses: Int = 0
+
+    @SerializedName("datetime")
+    var datetime: String = ""
+
 }
