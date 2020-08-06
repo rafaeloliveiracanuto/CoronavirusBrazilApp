79b4ea216d4d3c5f8cda10a7b190088388c3d2e4
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 12:33:05 -0300

    Create service and response

--- app/src/main/java/com/example/coronavirusbrazil/view/MainActivity.kt
+++ app/src/main/java/com/example/coronavirusbrazil/view/MainActivity.kt
@@ -14,6 +14,12 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.coronavirusbrazil.R
+import com.example.coronavirusbrazil.service.RetrofitClient
+import com.example.coronavirusbrazil.service.constants.Constants
+import com.example.coronavirusbrazil.service.model.CovidDataModel
+import com.example.coronavirusbrazil.service.repository.CovidDataService
+import retrofit2.Call
+import retrofit2.Callback
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,14 +28,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val remote = RetrofitClient.createService(CovidDataService::class.java)
+        val call: Call<CovidDataModel> = remote.getData(Constants.UF.RN)
+
+        val response = call.enqueue(object : Callback<CovidDataModel> {
+
+        })
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
