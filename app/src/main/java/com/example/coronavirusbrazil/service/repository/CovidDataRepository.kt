591671fb3562cca6d8cb18a02a7f5e2b722b0473
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-06 22:30:57 -0300

    Create getData function

--- app/src/main/java/com/example/coronavirusbrazil/service/repository/CovidDataRepository.kt
+++ app/src/main/java/com/example/coronavirusbrazil/service/repository/CovidDataRepository.kt
@@ -1,5 +1,17 @@
 package com.example.coronavirusbrazil.service.repository
 
-class CovidDataRepository {
+import android.content.Context
+import com.example.coronavirusbrazil.service.model.CovidDataModel
+import com.example.coronavirusbrazil.service.repository.remote.CovidDataService
+import com.example.coronavirusbrazil.service.repository.remote.RetrofitClient
+import retrofit2.Call
+
+class CovidDataRepository(val context: Context) {
+
+    private val mRemote = RetrofitClient.createService((CovidDataService::class.java))
+
+    fun getData(call: Call<CovidDataModel>) {
+        
+    }
 
 }
