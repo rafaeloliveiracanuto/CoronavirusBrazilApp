1601f4a0782eb80fa5799d9f912ba489190597a4
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-07 10:20:18 -0300

    Add LiveDatas, validation and repository

--- app/src/main/java/com/example/coronavirusbrazil/viewModel/NorthViewModel.kt
+++ app/src/main/java/com/example/coronavirusbrazil/viewModel/NorthViewModel.kt
@@ -5,7 +5,18 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.coronavirusbrazil.service.listener.ValidationListener
+import com.example.coronavirusbrazil.service.model.CovidDataModel
+import com.example.coronavirusbrazil.service.repository.CovidDataRepository
 
 class NorthViewModel(application: Application) : AndroidViewModel(application) {
-    
+    private val mCovidDataRepository = CovidDataRepository(application)
+
+    private val mValidation = MutableLiveData<ValidationListener>()
+    var validation: LiveData<ValidationListener> = mValidation
+
+    private val mGetData = MutableLiveData<CovidDataModel>()
+    var covidData: LiveData<CovidDataModel> = mGetData
+
+
 }
