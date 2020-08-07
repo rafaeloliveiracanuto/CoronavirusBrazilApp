e4d420bc93e6a1b133e7f437879af65d5dd62e7d
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-07 10:48:23 -0300

    Fix mViewModel

--- app/src/main/java/com/example/coronavirusbrazil/view/NorthFragment.kt
+++ app/src/main/java/com/example/coronavirusbrazil/view/NorthFragment.kt
@@ -7,26 +7,24 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.coronavirusbrazil.R
 import com.example.coronavirusbrazil.viewModel.NorthViewModel
 
 class NorthFragment : Fragment() {
 
-    private lateinit var homeViewModel: NorthViewModel
+    private lateinit var mViewModel: NorthViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(NorthViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        mViewModel = ViewModelProvider(this).get(NorthViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_north, container, false)
         val textView: TextView = root.findViewById(R.id.text_north)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        observe()
+
         return root
     }
 }
